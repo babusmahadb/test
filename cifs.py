1f0ee1a0d09f29b10b325992f81b315cdfe0d780
babusmahadb <reachbabuinbox@gmail.com>
2022-01-08 15:38:54 +0000

    Working cifs output

--- cifs.py
+++ cifs.py
@@ -1,9 +1,24 @@
-"""
-To get the CIFS Shares
-"""
+#! /usr/bin/env python3
 
-#Importing Modules
+"""
+ONTAP REST API Sample Scripts
 
+This script was developed by NetApp to help demonstrate NetApp
+technologies.  This script is not officially supported as a
+standard NetApp product.
+
+Purpose: Script to list volumes using ONTAP REST API.
+
+Usage: list_volumes.py [-h] -c CLUSTER -vs SVM_NAME [-u API_USER]
+                       [-p API_PASS]
+
+Copyright (c) 2020 NetApp, Inc. All Rights Reserved.
+Licensed under the BSD 3-Clause “New” or Revised” License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+https://opensource.org/licenses/BSD-3-Clause
+
+"""
 import base64
 import argparse
 from getpass import getpass
@@ -13,63 +28,79 @@ import requests
 import urllib3 as ur
 ur.disable_warnings()
 
-#getting the CIFS  Shares
+
 def get_volumes(cluster: str, svm_name: str, headers_inc: str):
     """Get Volumes"""
     url = "https://{}/api/storage/volumes/?svm.name={}".format(cluster, svm_name)
     response = requests.get(url, headers=headers_inc, verify=False)
     return response.json()
- 
- 
+
+
 def disp_vol(cluster: str, svm_name: str, headers_inc: str):
     """Display Volumes"""
     ctr = 0
     tmp = dict(get_volumes(cluster, svm_name, headers_inc))
     vols = tmp['records']
     tab = tt.Texttable()
-    header = (['Volume name', 'Volume UUID', 'SnapMirror(Y/N)', 'Source Path', ' Dest Path'])
+    header = (['Volume name', 'No. of CIFS Shares','CIFS Shares List','ACl'])
     tab.header(header)
-    tab.set_cols_width([18,50,25,15,15])
-    tab.set_cols_align(['c','c','c','c','c'])
+    tab.set_cols_width([18,20,60,60])
+    tab.set_cols_align(['c','c','c','c'])
+    
+    
+    CSEList=[]
     for volumelist in vols:
         ctr = ctr + 1
         vol = volumelist['name']
         uuid = volumelist['uuid']
-        url1 = "https://{}/api/storage/volumes/{}".format(cluster, uuid)
-        response = requests.get(url1, headers=headers_inc, verify=False)
-        vuid = response.json()
-        tmp2 = dict(vuid)
-        sv = tmp2['svm']
-        vsrv = sv['name']
-        svmvol = vsrv+"%3A"+vol
-        smurl = "https://{}/api/protocols/cifs/shares?fields=*&return_records=true&return_timeout=15".format(cluster)
-        response = requests.get(smurl, headers=headers_inc, verify=False)
-        vuid1 = response.json()
-        print(vuidl)
-        tmp3 = dict(vuid1)
-        dsmr = tmp3['records']
-        isp = scrp = desp = "NA"
-        for keys in dsmr:
-            chk = keys.get('source')
-            if chk is None:
-                scrp = "NA"
-                desp = "NA"
-                isp = "No"
-            else:
-                val = keys['source']
-                tval = dict(val)
-                scrp = tval['path']
-                dval = keys['destination']
-                dtval = dict(dval)
-                desp = dtval['path']
-                isp = "Yes"
-        tab.add_row([vol,uuid,isp,scrp,desp])
-        tab.set_cols_width([18,40,25,15,15])
-        tab.set_cols_align(['c','c','c','c','c'])
+        urlC="https://{}/api/protocols/cifs/shares/?volume={}".format(cluster,vol)
+        Cresponse = requests.get(urlC, headers=headers_inc, verify=False)
+        Cshare = Cresponse.json()
+        Crcout = Cshare['num_records']
+        CST=[]
+        CSList=[]
+        CSRList=[]
+        CSTList=[]
+        if Crcout!=0:
+            CSRecords=Cshare['records']
+            
+            for cs in CSRecords:
+                CSTemp=dict(cs)
+                CSN=CSTemp['name']
+                CST.append(CSN)
+                VS=CSTemp['svm']
+                VSN=VS['uuid']
+                
+                if "root" in vol:
+                    CSRT= CSN + "Share is in Root Volume"
+                    CSRList.append(CSRT)
+                else:
+                    CSI="https://{}/api/protocols/cifs/shares/{}/{}".format(cluster,VSN,CSN)
+                    CSIresponse=requests.get(CSI,headers=headers_inc, verify=False)
+                    CSID = CSIresponse.json()
+                    CSTN=CSID['name']
+                    CSTA=CSID['acls']
+                    CSTAT=[]
+                    for i in CSTA:
+                        CSTAC=i['permission']
+                        CSTUG=i['user_or_group']
+                        CSTMP="CIFS Share "+CSN+" has "+CSTUG+" Permission"+CSTAC
+                        #print(CSTMP)
+                        CSList.append(CSTMP)
+        if "root" in vol:
+            tab.add_row([vol,Crcout,CST,CSRList])
+        elif Crcout==0:
+            CT = "No Qtrees"
+            CSTList.append(CT)
+            tab.add_row([vol,Crcout,CST,CSTList])
+        else:     
+            tab.add_row([vol,Crcout,CST,CSList])
+        tab.set_cols_width([18,20,60,60])
+        tab.set_cols_align(['c','c','c','c'])
     print("Number of Volumes for this Storage Tenant: {}".format(ctr))
     setdisplay = tab.draw()
     print(setdisplay)
-
+    
 def parse_args() -> argparse.Namespace:
     """Parse the command line arguments from the user"""
 
@@ -110,3 +141,5 @@ if __name__ == "__main__":
         'content-type': "application/json",
         'accept': "application/json"
     }
+
+    disp_vol(ARGS.cluster, ARGS.svm_name, headers)
